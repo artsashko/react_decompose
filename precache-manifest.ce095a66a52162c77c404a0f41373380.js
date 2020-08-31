d96248f479c9b05ee844cb961c2b245635be0f3a
artsashko <artyomsashko@gmail.com>
2020-08-31 17:06:41 +0300

    Updates

---
+++ precache-manifest.ce095a66a52162c77c404a0f41373380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a2ae0cb4e2b02be88e2ff38b15f935",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9fa712208d36ed1a5f37",
+    "url": "./static/css/main.58b8e8f3.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa712208d36ed1a5f37",
+    "url": "./static/js/main.af2725cf.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
